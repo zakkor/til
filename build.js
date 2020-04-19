abb6efef13785668c533fc30d166c55cea49af35
Zakkor <edward.partenie@gmail.com>
2020-04-19 16:44:40 +0300

    styleripper: parse all css files before processing, integrate into build

--- build.js
+++ build.js
@@ -1,5 +1,47 @@
 const fs = require('fs')
 const path = require('path')
+const rip = require('./styleripper')
+
+fs.mkdir('./dist', async () => {
+	// Read head.html
+	const headbuf = fs.readFileSync('head.html')
+	htmlTemplate = htmlTemplate.replace('<%head%>', headbuf.toString())
+
+	const toProcess = collect('./pages', ['.html', '.css']).concat(collect('./styles', ['.css']))
+	const ripped = rip(toProcess)
+
+	let routes = {}
+	for (const page of ripped.html) {
+		const r = ('/' + removeFirstDir(page.path)).replace(/\/index.html$/, '/')
+		routes[r] = page.data
+	}
+
+
+	// First, create all output directories
+	walk('./pages', (filepath, isDir) => {
+		if (isDir) {
+			fs.mkdirSync(`./dist/${removeFirstDir(filepath)}`, { recursive: true })
+		}
+	})
+
+	for (const page of ripped.html) {
+		// Append routes except self to navigation template, and close the script tag.
+		let troutes = Object.assign({}, routes)
+		const r = ('/' + removeFirstDir(page.path)).replace(/\/index.html$/, '/')
+		delete(troutes, r)
+		console.log('troutes', troutes)
+		console.log('r', r)
+		troutes = JSON.stringify(troutes)
+
+		const navigation = navigationTemplate + `; const r = ${troutes} </script>`
+		let template = htmlTemplate.replace('<%navigation%>', navigation)
+		template = template.replace('<%body%>', page.data)
+		fs.writeFileSync(`./dist/${removeFirstDir(page.path)}`, template)
+	}
+
+	// Write CSS.
+	fs.writeFileSync(`./dist/${ripped.css.path}`, ripped.css.data)
+})
 
 let htmlTemplate = `<!DOCTYPE html>
 <html lang="en">
@@ -29,78 +71,50 @@ window.onpopstate = function() {
   go(location.pathname)
 }
 run()
-` 
+`
 
 // Call function on every file.
 function walk(d, fn) {
-  const dir = fs.opendirSync(d)
+	const dir = fs.opendirSync(d)
 
-  let dirent = null
-  while (dirent = dir.readSync()) {
-    if (dirent === null) {
-      break
-    }
+	let dirent = null
+	while (dirent = dir.readSync()) {
+		if (dirent === null) {
+			break
+		}
 
-    const full = path.join(d, dirent.name)
-    if (dirent.isDirectory()) {
-    	fn(full, true) // is dir
-      walk(full, fn)
-    } else {
-      fn(full, false) // is not dir
-    }
-  }
+		const full = path.join(d, dirent.name)
+		if (dirent.isDirectory()) {
+			fn(full, true) // is dir
+			walk(full, fn)
+		} else {
+			fn(full, false) // is not dir
+		}
+	}
 }
 
-fs.mkdir('./dist', async () => {
-	let routes = {}
-
-	// Create a map of each page and its contents
-	walk('./pages', (filepath, isDir) => {
-		if (isDir || !filepath.endsWith('/index.html')) {
-			return
-		}
-		
-		const buf = fs.readFileSync(filepath)
-		const r = ('/'+removeFirstDir(filepath)).replace(/\/index.html$/, '/')
-		routes[r] = buf.toString()
-	})
-
-	// Append routes to navigation template, and close the script.
-	routes = JSON.stringify(routes)
-	const navigation = navigationTemplate + `; const r = ${routes} </script>`
-	
-	// First, create all output directories
-	walk('./pages', (filepath, isDir) => {
-		if (!isDir) {
-			return
-		}
-
-		fs.mkdirSync(`./dist/${removeFirstDir(filepath)}`, { recursive: true })
-	})
-	
-	// Read head.html
-	const headbuf = fs.readFileSync('head.html')
-	htmlTemplate = htmlTemplate.replace('<%head%>', headbuf.toString())
-	
-	// Create all output pages
-	walk('./pages', (filepath, isDir) => {
+function collect(path, extensions) {
+	let files = []
+	walk(path, (filepath, isDir) => {
 		if (isDir) {
 			return
 		}
 
-		const buf = fs.readFileSync(filepath)
-
-		let template = htmlTemplate.replace('<%navigation%>', navigation)
-		
-		if (filepath.endsWith('/index.html')) {
-			template = template.replace('<%body%>', buf.toString())
-			fs.writeFileSync(`./dist/${removeFirstDir(filepath)}`, template)
-		} else {
-			// just copy file
-			fs.writeFileSync(`./dist/${removeFirstDir(filepath)}`, buf)
+		let ok = false
+		for (const ext of extensions) {
+			if (filepath.endsWith(ext)) {
+				ok = true
+				break
+			}
 		}
+		if (!ok) {
+			return
+		}
+
+		files.push(filepath)
 	})
-})
+	return files
+}
 
 function removeFirstDir(p) {
 	return p.replace(/.+?\//, '')
--- styleripper.js
+++ styleripper.js
@@ -1,91 +1,74 @@
 const fs = require('fs')
-const path = require('path')
 const csstree = require('css-tree')
 const htmlparse = require('node-html-parser')
 
-// A list of .html and .css files.
+// Takes a list of .html and .css files.
 // Ordering does not matter, but file extension does.
-const inputFiles = process.argv.slice(2)
+function rip(files) {
+	const htmlFiles = readFiles(files, '.html')
+	const cssFiles = readFiles(files, '.css')
+
+	// classnames contains information about the class name nodes appearing in all input files.
+	// `count` is the number of times each class name appears, created first when parsing HTML
+	// and updated when parsing CSS.
+	// `total` is the number of bytes occupied by this classname in both HTML and CSS files, created only after parsing CSS.
+	const classnames = {}
+	// renames contains information describing which nodes to rename to what
+	const rename = {
+		classnames: {} // { 'from': 'to' }
+	}
+
+	// Iterate through all HTML files and parse their contents,
+	// collecting information about different node types.
+	htmlFiles.forEach(f => {
+		f.ast = parseHTML(classnames, f.data)
+	})
+
+	// Iterate through all CSS files and parse their contents,
+	// collecting more information about different node types.
+	// Before walking the AST, all unused classnames are removed.
+	cssFiles.forEach(f => {
+		f.ast = parseCSS(classnames, f.data)
+	})
+
+	// Calculate the total byte size of each classname (count * name length) and collect it into an array so we can sort it.
+	let sorted = Object.entries(classnames)
+		.map(([name, sel]) => {
+			return { name, total: name.length * sel.count }
+		}).sort((a, b) => b.total - a.total)
+
+	// Afterwards, nodes are renamed and recorded in `rename`, and the resulting CSS is returned.
+	// Concatenate results into a string of all CSS file contents.
+	const css = {
+		path: 'built.css',
+		data: cssFiles.map(f => processCSS(sorted, rename, f.ast)).reduce((x, acc) => x + acc, ''),
+	}
+
+	// Go through HTML files again, and rewrite all nodes according to `rename`.
+	// Gather each file's rewritten content in an array.
+	const html = htmlFiles.map(f => {
+		processHTMLNodeChildren(rename, f.ast)
+		return {
+			path: f.name,
+			data: f.ast.toString(),
+		}
+	})
+
+	return {
+		html,
+		css,
+	}
+}
+
+module.exports = rip
+
 function readFiles(files, extension) {
 	return files.filter(f => f.endsWith(extension)).map(f => { return { name: f, data: fs.readFileSync(f, 'utf8') } })
 }
-const htmlFiles = readFiles(inputFiles, '.html')
-const cssFiles = readFiles(inputFiles, '.css')
 
-// classnames contains information about the class name nodes appearing in all input files.
-// `count` is the number of times each class name appears, created first when parsing HTML
-// and updated when parsing CSS.
-// `total` is the number of bytes occupied by this classname in both HTML and CSS files, created only after parsing CSS.
-const classnames = {}
-// renames contains information describing which nodes to rename to what
-const rename = {
-	classnames: {} // { 'from': 'to' }
-}
-
-// Iterate through all HTML files and parse their contents,
-// collecting information about different node types.
-htmlFiles.forEach(f => {
-	parseHTML(classnames, f.data)
-})
-
-// Iterate through all CSS files and parse their contents,
-// collecting more information about different node types.
-// Before walking the AST, all unused classnames are removed.
-// Afterwards, nodes are renamed and recorded in `rename`, and the resulting CSS is returned.
-// Concatenate results into a string of all CSS file contents.
-const css = cssFiles.map(f => {
-	return parseAndProcessCSS(classnames, rename, f.data)
-}).reduce((x, acc) => x + acc, '')
-
-// Write concatted CSS to file.
-const p = `./dist/built.css`
-fs.mkdirSync(path.dirname(p), { recursive: true })
-fs.writeFileSync(p, css)
-
-// Go through HTML files again, and rewrite all nodes according to `rename`.
-// Gather each file's rewritten content in an array.
-const html = htmlFiles.map(f => {
-	const node = htmlparse.parse(f.data)
-	processHTMLNodeChildren(rename, node)
-	return node.toString()
-})
-
-// Write each rewritten HTML to its own file.
-html.forEach((h, i) => {
-	const p = `./dist/${htmlFiles[i].name}`
-	fs.mkdirSync(path.dirname(p), { recursive: true })
-	fs.writeFileSync(p, h)
-})
-
-function parseHTML(classnames, data) {
-	const node = htmlparse.parse(data)
-	parseHTMLNodeChildren(classnames, node)
-}
-
-function parseHTMLNodeChildren(classnames, node) {
-	// Count each className occurrence.
-	if (node.classNames) {
-		for (const className of node.classNames) {
-			if (className in classnames) {
-				classnames[className].count++
-				continue
-			}
-			classnames[className] = { count: 1 }
-		}
-	}
-
-	for (const child of node.childNodes) {
-		parseHTMLNodeChildren(classnames, child)
-	}
-}
-
-function parseAndProcessCSS(classnames, rename, data) {
+function parseCSS(classnames, data) {
 	// Parse given CSS file into an AST.
 	const ast = csstree.parse(data)
-
-	function cleanName(n) {
-		return n.replace(/\\/g, '')
-	}
 
 	// Walk AST and remove rules in which the only selector is an unused class.
 	csstree.walk(ast, {
@@ -94,7 +77,7 @@ function parseAndProcessCSS(classnames, rename, data) {
 			node.prelude.children.each((selector, item, list) => {
 				// Remove any unused class selectors from SelectorList
 				selector.children.each((s) => {
-					if (s.type !== 'ClassSelector' || cleanName(s.name) in classnames) {
+					if (s.type !== 'ClassSelector' || cleanCSSIdentifier(s.name) in classnames) {
 						return
 					}
 
@@ -114,7 +97,7 @@ function parseAndProcessCSS(classnames, rename, data) {
 	csstree.walk(ast, {
 		visit: 'ClassSelector',
 		enter: function (node) {
-			const name = cleanName(node.name)
+			const name = cleanCSSIdentifier(node.name)
 			if (!(name in classnames)) {
 				throw new Error('encountered unused class selector when it should have been removed')
 			}
@@ -123,18 +106,16 @@ function parseAndProcessCSS(classnames, rename, data) {
 		}
 	})
 
-	// Calculate the total byte size of each classname (count * name length) and collect it into an array so we can sort it.
-	let sorted = Object.entries(classnames)
-		.map(([name, sel]) => {
-			return { name, total: name.length * sel.count }
-		}).sort((a, b) => b.total - a.total)
+	return ast
+}
 
+function processCSS(sorted, rename, ast) {
 	// For each selector in sorted order, walk through AST and rename each occurrence.
 	for (const [i, sel] of sorted.entries()) {
 		csstree.walk(ast, {
 			visit: 'ClassSelector',
 			enter: function (node) {
-				const name = cleanName(node.name)
+				const name = cleanCSSIdentifier(node.name)
 				if (name !== sel.name) {
 					return
 				}
@@ -147,6 +128,33 @@ function parseAndProcessCSS(classnames, rename, data) {
 	}
 
 	return csstree.generate(ast)
+}
+
+function cleanCSSIdentifier(n) {
+	return n.replace(/\\/g, '')
+}
+
+function parseHTML(classnames, data) {
+	const ast = htmlparse.parse(data)
+	parseHTMLNodeChildren(classnames, ast)
+	return ast
+}
+
+function parseHTMLNodeChildren(classnames, node) {
+	// Count each className occurrence.
+	if (node.classNames) {
+		for (const className of node.classNames) {
+			if (className in classnames) {
+				classnames[className].count++
+				continue
+			}
+			classnames[className] = { count: 1 }
+		}
+	}
+
+	for (const child of node.childNodes) {
+		parseHTMLNodeChildren(classnames, child)
+	}
 }
 
 function processHTMLNodeChildren(rename, node) {
@@ -179,7 +187,7 @@ function generateShortestName(idx) {
 	}
 
 	// fill with a-z
-	const ascii =	range(97, 123).map(c => String.fromCharCode(c))
+	const ascii = range(97, 123).map(c => String.fromCharCode(c))
 
 	let timesOver = 0
 	while (idx >= ascii.length) {
