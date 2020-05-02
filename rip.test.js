32675f2d97ac62a5824017c15972c16ff2c8fc3a
Zakkor <edward.partenie@gmail.com>
2020-05-03 02:48:50 +0300

    test: style changes

--- rip.test.js
+++ rip.test.js
@@ -1,69 +1,39 @@
 const rip = require('./dist/rip').rip
 
 test('does not remove used type selectors', () => {
-	const html = [
-		'<a> hi </a>',
-	]
-	const css = [
-		'a { padding: 4px; }',
-	]
-	const r = ripData(html, css, false)
-	expect(r).toEqual(['<style>a{padding:4px}</style><a> hi </a>'])
+	const html = [ '<a> hi </a>' ]
+	const css = [ 'a { padding: 4px; }' ]
+	expect(ripData(html, css, false)).toEqual(['<style>a{padding:4px}</style><a> hi </a>'])
 })
 
 test('does not remove used class selectors', () => {
-	const html = [
-		'<a class="used"> hi </a>',
-	]
-	const css = [
-		'.used { padding: 4px; }',
-	]
-	const r = ripData(html, css, false)
-	expect(r).toEqual(['<style>.used{padding:4px}</style><a class="used"> hi </a>'])
+	const html = [ '<a class="used"> hi </a>' ]
+	const css = [ '.used { padding: 4px; }' ]
+	expect(ripData(html, css, false)).toEqual(['<style>.used{padding:4px}</style><a class="used"> hi </a>'])
 })
 
 test('does not remove used id selectors', () => {
-	const html = [
-		'<a id="used"> hi </a>',
-	]
-	const css = [
-		'#used { padding: 4px; }',
-	]
-	const r = ripData(html, css, false)
-	expect(r).toEqual(['<style>#used{padding:4px}</style><a id="used"> hi </a>'])
+	const html = [ '<a id="used"> hi </a>' ]
+	const css = [ '#used { padding: 4px; }' ]
+	expect(ripData(html, css, false)).toEqual(['<style>#used{padding:4px}</style><a id="used"> hi </a>'])
 })
 
 test('removes unused type selectors', () => {
-	const html = [
-		'<a> hi </a>',
-	]
-	const css = [
-		'h1 { padding: 4px; }',
-	]
-	const r = ripData(html, css, false)
-	expect(r).toEqual(['<style></style><a> hi </a>'])
+	const html = [ '<a> hi </a>' ]
+	const css = [ 'h1 { padding: 4px; }' ]
+	expect(ripData(html, css, false)).toEqual(['<style></style><a> hi </a>'])
 })
 
 test('removes unused class selectors', () => {
-	const html = [
-		'<a> hi </a>',
-	]
-	const css = [
-		'.unused { padding: 4px; }',
-	]
-	const r = ripData(html, css, false)
-	expect(r).toEqual(['<style></style><a> hi </a>'])
+	const html = [ '<a> hi </a>' ]
+	const css = [ '.unused { padding: 4px; }' ]
+	expect(ripData(html, css, false)).toEqual(['<style></style><a> hi </a>'])
 })
 
 test('removes unused id selectors', () => {
-	const html = [
-		'<a> hi </a>',
-	]
-	const css = [
-		'#unused { padding: 4px; }',
-	]
-	const r = ripData(html, css, false)
-	expect(r).toEqual(['<style></style><a> hi </a>'])
+	const html = [ '<a> hi </a>' ]
+	const css = [ '#unused { padding: 4px; }' ]
+	expect(ripData(html, css, false)).toEqual(['<style></style><a> hi </a>'])
 })
 
 // Wraps each entry in a File object, then returns only the data from the ripped results.
